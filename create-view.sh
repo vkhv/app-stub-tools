9563a5169454b29218a5297b81f737a26000f94b
Vladislav Khvostov <vladkhvo@gmail.com>
2017-07-20 18:48:49 +0300

    Update create-view.sh

--- create-view.sh
+++ create-view.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+# Создать view
+# Первый аргумен - путь до папки с приложением
+# Второй аргумен - имя view
+pathToAppView="$1views/$2"
+pathToReactFile="$pathToAppView/$2.js"
+pathToReactServerFile="$pathToAppView/$2.server.js"
+
+mkdir $pathToAppView
+touch $pathToReactFile
+touch $pathToReactServerFile
